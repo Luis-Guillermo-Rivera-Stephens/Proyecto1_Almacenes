4484bb3500cb73e691427b334b2a5e79055fd3a1
Luis-Guillermo-Rivera-Stephens <148508811+Luis-Guillermo-Rivera-Stephens@users.noreply.github.com>
2025-03-04 08:53:12 -0600

    tablas en sql sin terminar

---
+++ Tables.sql
@@ -0,0 +1,50 @@
+CREATE TABLE DimCustomer (
+	CustomerID int NOT NULL PRIMARY KEY,
+	CompanyName varchar(50) NOT NULL,
+	ContactName varchar(50) NOT NULL,
+	City varchar(50),
+	Region varchar(50),
+	PostalCode varchar(50),
+	Country varchar(50)
+)
+
+CREATE TABLE DimProduct	(
+	ProductID int NOT NULL PRIMARY KEY,
+	ProductName varchar(50) NOT NULL,
+	SupplierName varchar(50),
+	CategoryName varchar(50),
+	QuantityPerUnit varchar(50)
+)
+
+CREATE TABLE DimTime (
+	TimeID INT NOT NULL PRIMARY KEY,
+	_year int NOT NULL,
+	_month int NOT NULL,
+	_day int NOT NULL
+)
+
+CREATE TABLE DimSubOrder (
+	OrderID int not null primary key 
+)
+
+CREATE TABLE FactOrder (
+	OrderID int not null primary key ,
+	ProductID int Null,
+	CustomerID nchar(10),
+	EmployeeID int,
+	OrderDate date,
+	TimeID int,
+	Qty int,
+	UnitPrice money,
+	Discount money,
+	TotalLine money,
+	ContarOrden float
+)
+
+--CREATE TABLE DimSubEntrada  (
+    --EntradaID 
+--)
+
+--CREATE TABLE DimSubSalida (
+    --SalidaID
+--)
